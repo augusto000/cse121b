139c551202ad6e959f92ba4ee2ef86a3db2bd4d5
augustoacha177 <augustoacha177@gmail.com>
2023-09-26 20:18:10 -0300

    W03 upload gamming Code

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -1,12 +1,26 @@
-/* W02-Task - Profile Home Page */
 
+
+/* Step 5 - Array */
+//let favFoods = ["pozole","entomatadas","mole","menudo", ];
+/*let favFoods = ["pozole "," entomatadas "," mole "," menudo "];
+let foodElement1=document.getElementById('food');
+let text = favFoods.join();
+let item = foodElement1.innerHTML =` <strong>${text}</strong>`;
+   console.log(item);
+
+//const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//let text = fruits.join();
+//console.log(text);
+//foodElement1.innerHTML = favFoods.join(',');
+//console.log('soy ',foodElement1);
+//let newFavoriteFood = "beans" ;
+*/
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
 let fullName ="Augusto Achá";
 let currentYear = new Date();
-const profilePicture ="augusto.jpg";
-
+const profilePicture ="images/augusto.jpg";
 /* Step 3 - Element Variables */
 let nameElement = document.getElementById('name');
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
@@ -15,7 +29,6 @@ nameElement.style.padding='1rem';
 
 let foodElement = document.getElementById('food');
 let yearElement = document.querySelector('#year');
-
 /* Step 4 - Adding Content */
 //Who returns an HTML collection? (HTML collection is an array)
 //The getElementsByTagName() Method
@@ -26,13 +39,12 @@ let img_ = document.getElementsByTagName('img');
 let imageElement = img_[0].setAttribute('src', profilePicture);
 img_[0].setAttribute('alt', `${profilePicture}`);
 img_[0].setAttribute('title', `${profilePicture}`);
-
 /* Step 5 - Array */
 let array = ['chicken soup','meat balls','rice','tacos'];
 let anotherFavoriteFood = 'pasta';
 
 let elements = array.length;
-console.log(typeof(index));
+
 array.map( (item, index)=>{ //mapping each element. Index the element
                            //check the last element and replace the , for the '.'  
                            index < elements-1 ? foodElement.innerHTML +=`<strong>${item}</strong>, `: foodElement.innerHTML +=` <strong>${item}</strong>.<br>`;   
@@ -44,13 +56,11 @@ array.map( (item, index)=>{
    });
 
 array.shift();   
-
 array.map( (item, index)=>{ 
     index < elements-1 ? foodElement.innerHTML +=`<em> ${item}</em>,`: foodElement.innerHTML +=` ${item}.<br>`;       
    });
 
 array.pop();
-
 array.map( (item, index)=>{ 
     index < elements-2 ? foodElement.innerHTML +=`<em>${item}</em>, `: foodElement.innerHTML +=` ${item}.<br>`;   
    });
