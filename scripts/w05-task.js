7ce75e140f61cbf4aa5c9162cb6307f972aa2496
Augusto Achá <40480509+augusto000@users.noreply.github.com>
2023-10-14 08:02:52 -0300

    Update w05-task.js
    wrong double declaration  about templeList

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,12 +1,12 @@
-
 /* W05: Programming Tasks */
 //https://byui-cse.github.io/cse121b-ww-course/resources/temples.json
 /* Declare and initialize global variables */
 const templesElement = document.querySelector('#temples');
 let templeList = [];
 /* async displayTemples Function */
-const displayTemples = (temples) => {
-             temples.forEach(temple => {
+let displayTemples = (temples) => {
+                //getTemples();
+                temples.forEach(temple => {
                 let article = document.createElement('article');                 
                 let templeName = document.createElement('h3');
                 templeName.textContent = temple.templeName;
@@ -30,30 +30,39 @@ const getTemples = async () =>{
        displayTemples(templeList);       
       };
 }
+const getTemples__ = async () =>{
+    let url = 'https://byui-cse.github.io/cse121b-ww-course/resources/temples.json';
+    const response = await fetch(url);
+    if (response.ok){
+        templeList = await response.json();
+        //console.log(data);
+       //displayTemples(templeList);       
+      };
+}
 /* reset Function */
 let reset = ()=> {      
-    templesElement.innerHTML = '';    
-};
-
+    templesElement.innerHTML ='';    
+}
 /* sortBy Function */
-const sortBy = (temples) => {
+const sortBy = (temples) => {    
     reset();
     let filter = document.querySelector("#sortBy").value;
     switch (filter) {
       case "utah":
-        console.log(filter);
-        displayTemples(temples.filter((temple)=>temple.location.includes("Utah")));
+          displayTemples(temples.filter((temple)=>temple.location.includes("Utah")));
           break;
       case "notutah":    
         console.log(filter);
-        displayTemples(temples.filter((temple)=>!temple.location.includes("Utah")));
           break;
-      case "all":
+      case "older":  //Built Before 1950
         console.log(filter);  
-          break;                      
+          break;
+      case "all":  
+          displayTemples(temples);
+          break;                                               
     }
 }
-    getTemples();
+getTemples();
     /* Event Listener */
 document.querySelector('#sortBy').addEventListener('change',() => sortBy(templeList));
 
